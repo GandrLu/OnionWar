6d87c173789a7fcf13e2bc1be3e4647a68f03e3a
GandrLu <acidyumyum@web.de>
2020-11-28 14:07:05 +0100

    Add photon and update code to make it work in multiplayer.

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,115 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    #region Private Serializable Fields
+
+    [SerializeField] private byte maxPlayerPerRoom = 4;
+
+    #endregion
+
+    #region Private Fields
+
+    // This clients game version number
+    string gameVersion = "1";
+
+    /// <summary>
+    /// Keep track of the current process. Since connection is asynchronous and is based on several callbacks from Photon,
+    /// we need to keep track of this to properly adjust the behavior when we receive call back by Photon.
+    /// Typically this is used for the OnConnectedToMaster() callback.
+    /// </summary>
+    bool isConnecting;
+
+    #endregion
+
+    #region Public Fields
+
+    [SerializeField] private GameObject controlPanel;
+    [SerializeField] private GameObject progressLabel;
+
+    #endregion
+
+    #region Unity CallBacks
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        controlPanel.SetActive(true);
+        progressLabel.SetActive(false);
+        //PhotonNetwork.SendRate = 40; // Default 20
+        //PhotonNetwork.SerializationRate = 40; // Default 10
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Connect()
+    {
+        controlPanel.SetActive(false);
+        progressLabel.SetActive(true);
+
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    #endregion
+
+    #region MonoBehaviourPunCallbacks Callbacks
+
+    public override void OnConnectedToMaster()
+    {
+        if (isConnecting)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+            PhotonNetwork.JoinRandomRoom();
+            isConnecting = false;
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        controlPanel.SetActive(true);
+        progressLabel.SetActive(false);
+
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by " +
+            "PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. " +
+            "No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayerPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("We load the default level");
+
+            PhotonNetwork.LoadLevel(1);
+        }
+    }
+
+    #endregion
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,6 +8,9 @@ public class Projectile : MonoBehaviour
     private string hitTag = "Enemy";
     [SerializeField]
     private float speed = 20f;
+    private float lifetime = 0f;
+    private float maxLifetime = 5f;
+
     void Start()
     {
         
@@ -15,7 +18,10 @@ public class Projectile : MonoBehaviour
 
     void Update()
     {
+        if (lifetime >= maxLifetime)
+            Destroy(gameObject);
         transform.position += transform.forward * speed * Time.deltaTime;
+        lifetime += Time.deltaTime;
     }
 
     //private void OnCollisionEnter(Collision other)
