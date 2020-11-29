3f60a330fa345b3a2e2dd470a74c48c880d5c579
GandrLu <acidyumyum@web.de>
2020-11-29 17:35:29 +0100

    Update practising target to be destructible and multiplayer synced.

---
+++ Assets/Scripts/Destructable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    [SerializeField] protected float lifepoints = 100f;
+    protected float currentLifepoints;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLifepoints = lifepoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InflictDamage(float damage)
+    {
+        currentLifepoints -= damage;
+        if (currentLifepoints <= 0)
+            Destruct();
+    }
+
+    public virtual void Destruct()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/HitBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    [SerializeField] string damagingTag = "Damaging";
+    [SerializeField] float defaultDamage;
+    private Destructable associatedDestructable;
+
+    void Start()
+    {
+        associatedDestructable = GetComponentInParent<Destructable>();
+        if (associatedDestructable == null)
+            throw new MissingReferenceException();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == damagingTag)
+        {
+            associatedDestructable.InflictDamage(defaultDamage);
+        }
+    }
+}
