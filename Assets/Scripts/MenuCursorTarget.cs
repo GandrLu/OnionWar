bb46d655544adc38bfea62826a4031e652dc77ee
GandrLu <acidyumyum@web.de>
2021-01-19 17:02:34 +0100

    Update main menu to better look. Add models with animators.

---
+++ Assets/Scripts/MenuCursorTarget.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCursorTarget : MonoBehaviour
+{
+    void Update()
+    {
+        var screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 3f);
+        transform.position = Camera.main.ScreenToWorldPoint(screenPosition);
+    }
+}
